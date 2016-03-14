ca5a8f2262ebbabd4f720cacf8928300a5c979e5
Tom Williams <tom.williams@hotschedules.com>
2016-03-14 16:12:03 -0400

    INFRA-1484 - update npm package name

--- manifests/init.pp
+++ manifests/init.pp
@@ -115,7 +115,7 @@ class nodejs(
     }
 
     default: {
-      package { 'npm':
+      package { 'nodejs-npm':
         name    => $nodejs::params::npm_pkg,
         ensure  => $version,
         require => Anchor['nodejs::repo']
